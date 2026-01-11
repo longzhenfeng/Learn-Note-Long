ba5d7ec78a99d390db5a70912def640dbda3684d
tjlongzhenfeng <1761473579@qq.com>
2026-01-11 20:19:44 +0800

    fix

--- packages/express-app/start-db.sh
+++ packages/express-app/start-db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p ./mongodb-data
-./mongodb-bin/bin/mongod --dbpath ./mongodb-data --port 27017 --bind_ip 127.0.0.1
+mongod --dbpath ./mongodb-data --port 27017 --bind_ip 127.0.0.1 &
+echo "MongoDB started in background"
